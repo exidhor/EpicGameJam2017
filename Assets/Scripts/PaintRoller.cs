3a7c010edfa854d4a2fefec37b360674380ef32c
Jipay <jipay.detiege@gmail.com>
2017-07-01 12:11:44 +0200

    Add animation rollers

--- Assets/Scripts/PaintRoller.cs
+++ Assets/Scripts/PaintRoller.cs
@@ -6,16 +6,49 @@ using UnityEngine;
 
 namespace EpicGameJam
 {
+    [RequireComponent(typeof(SpriteRenderer))]
     public class PaintRoller : MonoBehaviour
     {
         [SerializeField]
         private Color _traceColor;
 
+        private SpriteRenderer _spriteRenderer;
+
+        public List<Sprite> _animation;
+        public float speedAnimation;
+
+        private float _currentTime;
+        private int _currentIndex;
+
         public SpriteRenderer Traced;
+        public float Offset_x;
+
+        void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
 
         public void SetColor(Color color)
         {
             Traced.color = color;
+        }
+
+        void Update()
+        {
+            _currentTime += Time.deltaTime;
+
+            if (_currentTime > speedAnimation)
+            {
+                _currentTime = 0;
+                _currentIndex++;
+
+                if (_currentIndex >= _animation.Count)
+                {
+                    _currentIndex = 0;
+                }
+
+                _spriteRenderer.sprite = _animation[_currentIndex];
+            }
         }
 
         public void SetSize(float size)
@@ -26,7 +59,7 @@ namespace EpicGameJam
 
             Traced.transform.localScale = scale;
 
-            transform.position = Traced.bounds.center + new Vector3(Traced.bounds.extents.x, 0, 0);
+            transform.position = Traced.bounds.center + new Vector3(Traced.bounds.extents.x + Offset_x, 0, 0);
         }
     }
 }
